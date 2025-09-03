ec1b77c737ad21360b65a1cd9650c5578c8256e6
Rakhi <rakhichd@berkeley.edu>
2025-09-02 18:02:35 -0700

    added styling and images

--- rakhi_site/src/App.js
+++ rakhi_site/src/App.js
@@ -4,37 +4,24 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Project0 from './pages/Project0';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           Rakhi Chadalavada
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Projects
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 function App() {
   return (
     <Router>
-      <nav className="p-4 bg-gray-200 flex gap-4">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+      <nav className="toolbar">
+        <Link to="/" className="logo">
+          rakhi chadalavada
+        </Link>
+        <div className="nav-links">
+          <Link to="/">home</Link>
+          <Link to="/about">about</Link>
+          <Link to="/project0">project 0</Link>
+        </div>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/project0" element={<Project0 />} />
       </Routes>
     </Router>
   );
--- rakhi_site/src/pages/Project0.js
+++ rakhi_site/src/pages/Project0.js
@@ -1,3 +1,144 @@
 export default function Project0() {
-    return <h1>Project 0</h1>;
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          padding: "2rem",
+          gap: "2rem",
+          backgroundColor: "#CC4274",
+          minHeight: "100vh",
+        }}
+      >
+        <h2 style={{ color: "white", fontSize: "2rem" }}>part 1</h2>
+  
+        <div style={{ display: "flex", gap: "1rem" }}>
+          <div style={{ textAlign: "center" }}>
+            <img
+              src="/images/proj0/p1_close.jpeg"
+              alt="close up"
+              style={{
+                width: "250px",
+                height: "250px",
+                objectFit: "cover",
+                display: "inline-block",
+              }}
+            />
+            <div style={{ fontSize: "1.25rem", marginTop: "0.25rem", color: "white" }}>
+              close up
+            </div>
+          </div>
+  
+          <div style={{ textAlign: "center" }}>
+            <img
+              src="/images/proj0/p1_zoom.jpeg"
+              alt="zoom in"
+              style={{
+                width: "250px",
+                height: "250px",
+                objectFit: "cover",
+                display: "inline-block",
+              }}
+            />
+            <div style={{ fontSize: "1.25rem", marginTop: "0.25rem", color: "white" }}>
+              zoomed in
+            </div>
+          </div>
+        </div>
+
+        <h2 style={{ color: "white", fontSize: "2rem" }}>part 2</h2>
+  
+        <div style={{ display: "flex", gap: "1rem" }}>
+          <div style={{ textAlign: "center" }}>
+            <img
+              src="/images/proj0/p2_close.jpeg"
+              alt="close up"
+              style={{
+                width: "250px",
+                height: "250px",
+                objectFit: "cover",
+                display: "inline-block",
+              }}
+            />
+            <div style={{ fontSize: "1.25rem", marginTop: "0.25rem", color: "white" }}>
+              close up
+            </div>
+          </div>
+  
+          <div style={{ textAlign: "center" }}>
+            <img
+              src="/images/proj0/p2_zoom.jpeg"
+              alt="zoom in"
+              style={{
+                width: "250px",
+                height: "250px",
+                objectFit: "cover",
+                display: "inline-block",
+              }}
+            />
+            <div style={{ fontSize: "1.25rem", marginTop: "0.25rem", color: "white" }}>
+              zoomed in
+            </div>
+          </div>
+        </div>
+
+        <div style={{ display: "flex", gap: "1rem" }}>
+          <div style={{ textAlign: "center" }}>
+            <img
+              src="/images/proj0/p2_0_close.jpeg"
+              alt="close up"
+              style={{
+                width: "250px",
+                height: "250px",
+                objectFit: "cover",
+                display: "inline-block",
+              }}
+            />
+            <div style={{ fontSize: "1.25rem", marginTop: "0.25rem", color: "white" }}>
+              close up
+            </div>
+          </div>
+  
+          <div style={{ textAlign: "center" }}>
+            <img
+              src="/images/proj0/p2_0_zoom.jpeg"
+              alt="zoom in"
+              style={{
+                width: "250px",
+                height: "250px",
+                objectFit: "cover",
+                display: "inline-block",
+              }}
+            />
+            <div style={{ fontSize: "1.25rem", marginTop: "0.25rem", color: "white" }}>
+              zoomed in
+            </div>
+          </div>
+        </div>
+
+        <h2 style={{ color: "white", fontSize: "2rem" }}>part 3</h2>
+  
+        <div style={{ display: "flex", gap: "1rem" }}>
+          <div style={{ textAlign: "center" }}>
+            <img
+              src="/images/proj0/dollyzoom.gif"
+              alt="dolly zoom"
+              style={{
+                width: "500px",
+                height: "500px",
+                objectFit: "cover",
+                display: "inline-block",
+              }}
+            />
+            <div style={{ fontSize: "1.25rem", marginTop: "0.25rem", color: "white" }}>
+              the dolly zoom
+            </div>
+          </div>
+        </div>
+
+      </div>
+    );
   }
+  
